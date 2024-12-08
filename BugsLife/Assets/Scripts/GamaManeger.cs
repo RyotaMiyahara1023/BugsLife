beacf61e39a68dd870f516632a1ee285a7a43134
RyotaMiyahara5164 <98142783+RyotaMiyahara1023@users.noreply.github.com>
2024-12-08 15:49:15 +0900

    Game画面の概形を作成

---
+++ BugsLife/Assets/Scripts/GamaManeger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamaManeger : MonoBehaviour
+{
+    [SerializeField] GameObject _pause;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pause()
+    {
+        _pause.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        _pause.SetActive(false);
+    }
+}
